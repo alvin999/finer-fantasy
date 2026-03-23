4d8e4d38939c4185b610152769bb79f069707f83
alvin999 <bingoppp@gmail.com>
2026-03-23 12:43:49 +0800

    fix: update Astro config and GitHub Actions for proper deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://alvin999.github.io',
+  base: '/finer-fantasy',
   integrations: [react()]
 });
